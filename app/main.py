09c777cd0643d4e6e4c07bb580dc45fdf32d0651
Julia Alexeeva <julia4406@gmail.com>
2024-12-13 15:08:53 +0200

    Removed docstrings

--- app/main.py
+++ app/main.py
@@ -2,7 +2,6 @@ from abc import ABC
 
 
 class IntegerRange:
-    """ class - descriptor for parameters limitations """
     def __init__(self, min_amount: int, max_amount: int) -> None:
         self.min_amount = min_amount
         self.max_amount = max_amount
@@ -22,7 +21,6 @@ class IntegerRange:
 
 
 class Visitor:
-    """  class that is responsible for the user's personal data  """
     def __init__(self, name: str, age: int, weight: int, height: int) -> None:
         self.name = name
         self.age = age
@@ -38,14 +36,12 @@ class SlideLimitationValidator(ABC):
 
 
 class ChildrenSlideLimitationValidator(SlideLimitationValidator):
-    """  it's a limitation class validators for slides  """
     age = IntegerRange(4, 14)
     height = IntegerRange(80, 120)
     weight = IntegerRange(20, 50)
 
 
 class AdultSlideLimitationValidator(SlideLimitationValidator):
-    """  it's a limitation class validators for slides  """
     age = IntegerRange(14, 60)
     height = IntegerRange(120, 220)
     weight = IntegerRange(50, 120)
@@ -61,7 +57,6 @@ class Slide:
         self.limitation_class = limitation_class
 
     def can_access(self, client: Visitor) -> bool:
-        """  checks whether a Visitor can use the slide  """
         try:
             self.limitation_class(age=client.age,
                                   height=client.height,
